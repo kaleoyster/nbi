b5846706f21f8267920141a44fd91c72c91b2c86
Akshay Kale <akshaykale915@gmail.com>
2019-10-04 04:52:39 -0500

    added manipulation files

---
+++ nbi-datacenterhub/chef.py
@@ -0,0 +1,44 @@
+"""
+Author: Akshay Kale
+Description: This class contains all the function required to produce the upload able 
+document on 
+Python Version: 3.6.2
+"""
+
+class DataChef:
+    def __init__(self):
+        """ 
+         Description:
+         
+         input:
+         return:
+
+        """
+        pass
+
+    def get_ingridients(URL):
+        """
+         Descriptions:
+         
+         input:
+         return:
+        """
+        pass
+
+    def save_ingridents():
+        """
+         Descriptions:
+         
+         input:
+         return:
+        """
+        pass
+    def season_material():
+        """
+         Descriptions:
+
+         imput:
+         returns:
+        """
+        pass
+
