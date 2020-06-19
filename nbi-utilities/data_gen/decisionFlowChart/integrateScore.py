fdfeca664c6855f9b27905b19e0f8724e91ab72b
Akshay Kale <akshaykale915@gmail.com>
2020-06-19 16:18:13 -0500

    fixed base line difference method categorization function

--- nbi-utilities/data_gen/decisionFlowChart/integrateScore.py
+++ nbi-utilities/data_gen/decisionFlowChart/integrateScore.py
@@ -8,7 +8,7 @@ import os
 import csv
 from collections import defaultdict
 from collections import namedtuple
-
+import numpy as np
 
 __author__ = 'Akshay Kale'
 __copyright__='GPL'
@@ -29,16 +29,44 @@ def read_csv(path, csvfile):
     return listOfRecords
 
 
-def to_csv(listOfNewRecords):
+def to_csv(listOfNewRecords, csvfile, fieldnames):
     with open(csvfile, 'w') as csvFile:
-        csvWriter = csv.writer()
+        csvWriter = csv.writer(csvFile, delimiter=',')
         csvWriter.writerow(fieldnames)
         for record in listOfNewRecords:
             csvWriter.writerow(record)
     return True
 
 
-def fetchBSDScore(listOfBSDRecords):
+def calc_bds_cat(newRecords):
+    bsdScores = [rec[-1] for rec in newRecords]
+    listOfCat = list()
+    tempScores = list()
+
+    for score in bsdScores:
+        try:
+            tempScores.append(float(score))
+        except:
+            tempScores.append(None)
+
+    mean = np.mean(list(filter(None, tempScores)))
+    std = np.std(list(filter(None, tempScores)))
+
+    for rec, score in zip(newRecords, tempScores):
+        if score != None:
+            if score > (mean + std):
+                rec.append('Good')
+            elif score > (mean - std):
+                rec.append('Bad')
+            else:
+                rec.append('Average')
+        else:
+            rec.append(None)
+        listOfCat.append(rec)
+    return listOfCat
+
+
+def fetch_BSD_score(listOfBSDRecords):
     bridgeBDSDict = defaultdict()
     for record in listOfBSDRecords:
         structNum = record.StructureNumber[:-2]
@@ -47,7 +75,7 @@ def fetchBSDScore(listOfBSDRecords):
     return bridgeBDSDict
 
 
-def updateRecord(listOfNDOTRecords, bridgeBDSDict):
+def update_record(listOfNDOTRecords, bridgeBDSDict):
     structureNumbers = list()
     listOfBDS = list()
     listOfNewRecords = list()
@@ -69,15 +97,21 @@ def updateRecord(listOfNDOTRecords, bridgeBDSDict):
 def main():
     path = '../../../../data/nbi/'
     os.chdir(path)
+
     csvNDOTFile = 'intervention.csv'
     csvBSDFile = '06-20-19-thesis-dataset_allstates_allstates.csv'
+    csvNewFile = 'intervention_bds.csv'
+
     listOfNDOTRecords = read_csv(path, csvNDOTFile)
     listOfBSDRecords = read_csv(path, csvBSDFile)
 
-    bridgeBDSDict = fetchBSDScore(listOfBSDRecords)
-    newRecords = updateRecord(listOfNDOTRecords, bridgeBDSDict)
-    print(newRecords)
-    #to_csv(newRecords)
-    # integrate newRecord with the Yes or No
+    bridgeBDSDict = fetch_BSD_score(listOfBSDRecords)
+    newRecords = update_record(listOfNDOTRecords, bridgeBDSDict)
+    newRecords = calc_bds_cat(newRecords)
+
+    fieldnames = ['structureNumber', 'intervention', 'score', 'category']
+    csvWrite = to_csv(newRecords, csvNewFile, fieldnames)
+
+
 if __name__=='__main__':
     main()
