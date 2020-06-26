75f40bcb0f145dedf7f631d84ceafcafb62bd675
Akshay Kale <akshaykale915@gmail.com>
2020-06-26 12:12:11 -0500

    added counting function for positive cases and negative cases

--- nbi-utilities/data_gen/decisionFlowChart/validation.py
+++ nbi-utilities/data_gen/decisionFlowChart/validation.py
@@ -5,7 +5,7 @@ import csv
 import os
 from collections import namedtuple
 from collections import defaultdict
-
+from collections import Counter
 
 __author__ = 'Akshay Kale'
 __copyrigth__ = 'GPL'
@@ -31,7 +31,7 @@ def read_main_dataset(csvFile):
     return listOfRecords
 
 
-def read_rf_results(csvRfFile):
+def read_gt_results(csvRfFile):
     listOfRfIntervention = list()
     with open(csvRfFile, 'r') as csvfile:
         csvReader = csv.reader(csvfile, delimiter=',')
@@ -41,7 +41,8 @@ def read_rf_results(csvRfFile):
             rec = record(*row)
             structNum = rec.structureNumber
             intervention = rec.intervention
-            listOfRfIntervention.append([structNum, intervention])
+            rfIntervention =  rec.rfIntervention
+            listOfRfIntervention.append([structNum, intervention, rfIntervention])
     return listOfRfIntervention
 
 
@@ -62,6 +63,88 @@ def create_dict(listOfRecords):
 
     return structDeck, structSub, structSup
 
+def both_positive(record):
+    intervention = record[1]
+    rfResult = record[2]
+    groundTruth = record[3]
+
+    if intervention == 'Yes' and rfResult == 'Yes' and groundTruth == 'Yes':
+        return True
+    else:
+        return False
+
+def rf_correct(record):
+    intervention = record[1]
+    rfResult = record[2]
+    groundTruth = record[3]
+
+    if intervention == 'No' and rfResult == 'Yes' and groundTruth == 'Yes':
+        return True
+    else:
+        return False
+
+def flow_correct(record):
+    intervention = record[1]
+    rfResult = record[2]
+    groundTruth = record[3]
+
+    if intervention == 'Yes' and rfResult == 'No' and groundTruth == 'Yes':
+        return True
+    else:
+        return False
+
+
+def both_negative(record):
+    intervention = record[1]
+    rfResult = record[2]
+    groundTruth = record[3]
+
+    if intervention == 'No' and rfResult == 'No' and groundTruth == 'Yes':
+        return True
+    else:
+        return False
+
+def integrate(listOfRfIntervention, structDeck):
+    bothPositiveList = list()
+    rfCorrectList = list()
+    flowCorrectList = list()
+    bothNegativeList = list()
+    for record in listOfRfIntervention:
+        structNum = record[0]
+        record.append(structDeck.get(structNum))
+        # Recoding flow chart intervention
+        if record[1][:4] == 'None':
+            record[1] = 'No'
+        else:
+            record[1] = 'Yes'
+
+        # Recoding random forest results
+        if record[2] == '':
+            record[2] = None
+        elif record[2] == 'yes':
+            record[2] = 'Yes'
+        else:
+            record[2] = 'No'
+
+        # Record both positive
+        if both_positive(record):
+            bothPositiveList.append(record)
+
+        elif rf_correct(record):
+            rfCorrectList.append(record)
+
+        elif flow_correct(record):
+            flowCorrectList.append(record)
+
+        elif both_negative(record):
+            bothNegativeList.append(record)
+
+        else:
+            continue
+
+    return bothPositiveList, rfCorrectList, flowCorrectList, bothNegativeList
+
+
 
 def main():
     path = '../../../../data/trees/decision-tree-dataset/'
@@ -69,13 +152,18 @@ def main():
     os.chdir(path)
 
     pathIntervention = '../../nbi/'
-    csvRfFile = 'intervention_bds.csv'
+    csvGtFile = 'intervention_bds.csv'
 
     listOfRecords = read_main_dataset(csvFile)
     os.chdir(pathIntervention)
-    listOfRfIntervention = read_rf_results(csvRfFile)
+    listOfGtIntervention = read_gt_results(csvGtFile)
+    # listOfRecords -  deck, superstructure, substructure
     structDeck, structSub, structSup = create_dict(listOfRecords)
-    print(listOfRfIntervention)
+    ## RF intervention
+    #- Structure Number, Flow Chart, Random Forest, label
+    #print(listOfGtIntervention)
+    pos, rf, fl, neg = integrate(listOfGtIntervention, structDeck)
+    print(neg)
     #print(listOfRecords)
 
 
